25a65b13a21c183599ac73108ae06a019ce0eec5
YuliiaGeraskina <julia.ulik@gmail.com>
2015-03-23 13:19:36 +0200

    Update Human.java

--- Human.java
+++ Human.java
@@ -1,3 +1,5 @@
+import java.util.Comparator;
+
 
 class Human implements Comparable {
 private int age;
@@ -14,5 +16,16 @@ if (age > h.age) return 1;
 else if (age < h.age)return -1;
 else
 return 0; 
+}
+}
+
+class HumanComparator implements Comparator<Object> {
+@Override
+public int compare(Object o1, Object o2) {
+Human h1 = (Human)o1;
+Human h2 = (Human)o2;
+if (h1.getAge() > h2.getAge())return 1;
+else if (h1.getAge() < h2.getAge())return -1;
+else return 0; 
 }
 }
