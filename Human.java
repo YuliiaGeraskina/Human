1c154322373556e6795501428f07b37abf9f4105
YuliiaGeraskina <julia.ulik@gmail.com>
2015-03-23 13:17:41 +0200

    Update and rename .gitignore to Human.java

---
+++ Human.java
@@ -0,0 +1,18 @@
+
+class Human implements Comparable {
+private int age;
+public Human(int age) {
+this.age = age;
+}
+public int getAge() {
+return age;
+}
+@Override
+public int compareTo(Object another) {
+Human h = (Human) another;
+if (age > h.age) return 1;
+else if (age < h.age)return -1;
+else
+return 0; 
+}
+}
