a56c94f9f3f22765cc31f83ccd09958da0f842bf
YuliiaGeraskina <julia.ulik@gmail.com>
2015-03-23 13:20:17 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Human[] list = { new Human(40), new Human(20), new Human(3),
+				new Human(7) };
+
+		Arrays.sort(list, new HumanComparator().reversed());
+		for (Human h : list)
+			System.out.println(h.getAge());
+	}
+}
